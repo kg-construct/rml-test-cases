5180e32065fff2ce2f11edcb5c44e54eff9af6b0
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-01-17 09:53:01 +0100

    initial commit

---
+++ RMLTC0001b-PostgreSQL/resource.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS student;
+CREATE TABLE student (
+  name VARCHAR(50)
+);
+INSERT INTO student values ('Venus');
---
+++ RMLTC0003b-PostgreSQL/resource.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS student;
+CREATE TABLE student (
+  id INTEGER,
+  firstname VARCHAR(50),
+  lastname VARCHAR(50)
+);
+INSERT INTO student values ('10', 'Venus', 'Williams');
